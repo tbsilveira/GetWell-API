4fce23877a5504e29b2d86879efe8a35ab9b84d9
tbsilveira <tiagobsilveira@gmail.com>
2023-10-23 07:06:55 +0200

    JWT token validation added

---
+++ src/main/java/getwell/api/infra/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package getwell.api.infra.security;
+
+import getwell.api.domain.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Authorization Header - token recovery
+        var tokenJWT = recoverytoken(request);
+
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var user = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverytoken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
--- src/main/java/getwell/api/infra/security/TokenService.java
+++ src/main/java/getwell/api/infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package getwell.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import getwell.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error on create JWT token", exception);
+        }
+    }
+
+    //Method for JWT Token validation
+    public String getSubject(String tokenJTW) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return String.valueOf(JWT.require(algorithm)
+                    .withIssuer("API GetWell")
+                    .build()
+                    .verify(tokenJTW)
+                    .getSubject());
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("JWT Token invalid or expired!");
         }
     }
 
